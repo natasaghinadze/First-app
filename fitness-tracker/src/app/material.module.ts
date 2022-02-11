81419d2dff27e5cf70d193e1a55e32bd4e9b8aea
natasaghinadze <85290787+natasaghinadze@users.noreply.github.com>
2022-02-11 22:25:30 +0400

    Tabs Component

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -9,7 +9,7 @@ import { MatNativeDateModule } from "@angular/material/core";
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
-
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   imports: [
@@ -23,6 +23,7 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
+    MatTabsModule,
   ],
   exports: [
     MatButtonModule,
@@ -35,6 +36,7 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
+    MatTabsModule,
   ],
 })
 export class MaterialModule {}
