4957f37e4adc0fbd08a28d21aebe4d68a35eec96
natasaghinadze <85290787+natasaghinadze@users.noreply.github.com>
2022-02-14 15:59:10 +0400

    Adding a Form

--- fitness-tracker/src/app/training/training.service.ts
+++ fitness-tracker/src/app/training/training.service.ts
@@ -1,6 +1,10 @@
+import { Subject } from "rxjs";
+
 import { Exercise } from "./exercise.model";
 
 export class TrainingService {
+  exerciseChanged = new Subject<Exercise>();
+
   private availableExerccises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
@@ -8,7 +12,16 @@ export class TrainingService {
     { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 },
   ];
 
+  private runningExercise: Exercise | any;
+
   getAvailableExerccises(){
     return this.availableExerccises.slice();
+  }
+
+  startExercise(selectedId: string){
+    this.runningExercise = this.availableExerccises.find(
+      ex => ex.id === selectedId
+      );
+    this.exerciseChanged.next({...this.runningExercise})
   }
 }
