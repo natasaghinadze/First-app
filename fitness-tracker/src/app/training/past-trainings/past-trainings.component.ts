cbf46585e4a5835eb6c7558d0f9bd4f12b171024
natasaghinadze <85290787+natasaghinadze@users.noreply.github.com>
2022-02-15 15:48:51 +0400

    edd pagination to the data table

--- fitness-tracker/src/app/training/past-trainings/past-trainings.component.ts
+++ fitness-tracker/src/app/training/past-trainings/past-trainings.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Exercise } from '../exercise.model';
@@ -7,27 +8,29 @@ import { TrainingService } from '../training.service';
 @Component({
   selector: 'app-past-trainings',
   templateUrl: './past-trainings.component.html',
-  styleUrls: ['./past-trainings.component.css']
+  styleUrls: ['./past-trainings.component.css'],
 })
-export class PastTrainingsComponent implements OnInit, AfterViewInit{
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
 
-  @ViewChild(MatSort) sort:any | MatSort;
+  @ViewChild(MatSort) sort: any | MatSort;
+  @ViewChild(MatPaginator) paginator: any | MatPaginator;
 
   constructor(private trainingService: TrainingService) {}
 
   ngOnInit(): void {
-    this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
+    this.dataSource.data =
+      this.trainingService.getCompletedOrCancelledExercises();
   }
 
   ngAfterViewInit(): void {
-      this.dataSource.sort = this.sort;
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
-  doFilter(event:Event){
+  doFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-
 }
